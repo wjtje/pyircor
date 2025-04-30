464751843dd26822518489ad71824059e990ad5f
Wouter van der Wal <33957974+wjtje@users.noreply.github.com>
2025-04-30 09:31:52 +0200

    Bump package version and updated dependencies

--- pyircor/check.py
+++ pyircor/check.py
@@ -1,5 +1,5 @@
 import numpy as np
-from collections import Iterable
+from collections.abc import Iterable
 
 
 def is_numeric(obj):
--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'numpy==1.18.2',
-    'numba==0.49.0',
-    'scipy==1.4.1'
+    'numpy==2.2.5',
+    'numba==0.61.2',
+    'scipy==1.15.2'
 ]
 
 setup_requirements = [ ]
@@ -23,16 +23,12 @@ test_requirements = [ ]
 setup(
     author="Jaehun Kim",
     author_email='jaehun.j.kim@gmail.com',
-    python_requires='>=3.6, <=3.8',
+    python_requires='>=3.11',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
     ],
     description="Python implementation of the R package `ircor`",
     # entry_points={
@@ -51,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/eldrin/pyircor',
-    version='0.2.0',
+    version='0.2.1',
     zip_safe=False,
 )
